8bc984749c4fdb6a44c2cbe5433f496ebce58405
hosuk <kirklayer@gmail.com>
2020-08-25 14:30:33 +0900

    merge test

--- src/pages/fifth.js
+++ src/pages/fifth.js
@@ -28,10 +28,59 @@ export default function Main() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     containerRef.current.appendChild(renderer.domElement);
 
+
+    var singleGeo = new THREE.Geometry();
+
+
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshNormalMaterial();
-    cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    var material = new THREE.MeshNormalMaterial({wireframe:true});
+    var tcube1 = new THREE.Mesh(geometry, material);
+    //scene.add(cube);
+
+    var geometry2 = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshNormalMaterial({wireframe:true});
+    // var geo3 = geometry2.mergeMesh(cube)
+    var tcube2 = new THREE.Mesh(geometry2, material);
+    tcube2.position.set(0.5,0,0)
+
+      tcube1.updateMatrix();
+      singleGeo.mergeMesh(tcube1)
+      tcube2.updateMatrix();
+      singleGeo.mergeMesh(tcube2)
+      singleGeo.mergeVertices();
+
+      scene.add(new THREE.Mesh(singleGeo, material))
+
+
+    
+    //scene.add(cube);
+
+    var x = 0,
+      y = 0,
+      z=0
+    var heartShape = new THREE.Shape();
+
+    heartShape.moveTo(x + 5, y + 5, z+5);
+    heartShape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y);
+    heartShape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7);
+    heartShape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19);
+    heartShape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7);
+    heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
+    heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
+
+    console.log(heartShape)
+    console.log(heartShape[0])
+
+    // 이함수를 사용하는것이외에 선택지는 없다 ..... 
+    console.log(heartShape.extractPoints(16))
+
+    var geometry = new THREE.ShapeGeometry(heartShape, 16);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe:true });
+    var mesh = new THREE.Mesh(geometry, material);
+    //scene.add(mesh);
+
+    //cube.mergeMesh(mesh)
+
     camera.position.z = 5;
 
     cameraControls = new CameraControls(camera, renderer.domElement);
@@ -39,8 +88,8 @@ export default function Main() {
 
   function Animate() {
     requestAnimationFrame(Animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
     const delta = clock.getDelta();
     // const hasControlsUpdated = cameraControls.update(delta);
